96bb947f7af8920c6a8d4a0f9a17880486ea67fb
mattialicausi <licausivitomattia@gmail.com>
2023-03-24 15:11:59 +0100

    aggiunto assertTrows per testare i metodi

--- src/test/java/calculator/tester/CalculatorTest.java
+++ src/test/java/calculator/tester/CalculatorTest.java
@@ -1,6 +1,5 @@
 package calculator.tester;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,20 +9,24 @@ class CalculatorTest {
     @Test
     void add() {
         assertEquals(8.0, Calculator.add(4.0f, 4.0f) );
+        assertThrows(IllegalArgumentException.class, () ->Calculator.divide(-4.0f, 2.2f));
     }
 
     @Test
     void subtract() {
         assertEquals(0.0, Calculator.subtract(4.0f, 4.0f) );
+        assertThrows(IllegalArgumentException.class, () ->Calculator.divide(7.5f, -7.5f));
     }
 
     @Test
     void divide() {
         assertEquals(3.0, Calculator.divide(6.0f, 2.0f) );
+        assertThrows(IllegalArgumentException.class, () ->Calculator.divide(7.0f, -2.2f));
     }
 
     @Test
     void multiply() {
         assertEquals(64.0, Calculator.multiply(8.0f, 8.0f) );
+        assertThrows(IllegalArgumentException.class, () ->Calculator.divide(4.0f, -8.2f));
     }
 }
